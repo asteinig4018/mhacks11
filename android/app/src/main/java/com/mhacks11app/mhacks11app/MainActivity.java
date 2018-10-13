79884a0bc3000132c25aaa13141a8dd1304f1567
AlvinHermans <alvinhermans123@gmail.com>
2018-10-13 08:46:32 -0400

    Finished frontend for locationcreate + server class

--- android/app/src/main/java/com/mhacks11app/mhacks11app/MainActivity.java
+++ android/app/src/main/java/com/mhacks11app/mhacks11app/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -17,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-
 
     }
     public void nextActivity(View view){
---
+++ android/app/src/main/java/com/mhacks11app/mhacks11app/Servers.java
@@ -0,0 +1,13 @@
+package com.mhacks11app.mhacks11app;
+
+import android.widget.CheckBox;
+import android.view.View;
+
+public class Servers {
+
+    boolean checked = ((CheckBox) view).isChecked();
+    switch(view.getId()){
+        
+
+    }
+}
