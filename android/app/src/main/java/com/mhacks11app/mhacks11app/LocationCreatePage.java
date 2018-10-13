ca79cef37516a9323cf8619fd19eb3774e20e09b
AlvinHermans <alvinhermans123@gmail.com>
2018-10-13 07:09:30 -0400

    Finished GUI of locationCreate page

--- android/app/src/main/java/com/mhacks11app/mhacks11app/LocationCreatePage.java
+++ android/app/src/main/java/com/mhacks11app/mhacks11app/LocationCreatePage.java
@@ -4,10 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class LocationCreatePage extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_create_page);
+
     }
 }
