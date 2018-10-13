d8e4b5b09cf1fb06b61c4f78ac8467de49070a34
asteinig4018 <asteinig18@gmail.com>
2018-10-13 09:34:36 -0400

    added structure

--- android/app/src/main/java/com/mhacks11app/mhacks11app/Servers.java
+++ android/app/src/main/java/com/mhacks11app/mhacks11app/Servers.java
@@ -3,11 +3,25 @@ package com.mhacks11app.mhacks11app;
 import android.widget.CheckBox;
 import android.view.View;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Servers {
+    //define class
 
-    boolean checked = ((CheckBox) view).isChecked();
-    switch(view.getId()){
-        
 
+    //on push of submit button
+
+    public void submitData() {
+
+        //create new list object
+        Map<String,Object> data1=new HashMap<>();
+
+
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()) {
+
+
+        }
     }
 }
