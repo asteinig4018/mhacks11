95df8585e9b532838f0baf150bf0f1b0b47f655b
AlvinHermans <alvinhermans123@gmail.com>
2018-10-13 13:29:15 -0400

    Fixed names

--- android/app/src/main/java/com/mhacks11app/mhacks11app/LocationCreatePage.java
+++ android/app/src/main/java/com/mhacks11app/mhacks11app/LocationCreatePage.java
@@ -6,7 +6,6 @@ import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -73,7 +72,7 @@ public class LocationCreatePage extends AppCompatActivity {
         CheckBox otherCheck = findViewById(R.id.other_Check);
         boolean isOther = otherCheck.isChecked();
 
-        CheckBox sunriseCheck = findViewById(R.id.sunrise_Check);
+        CheckBox sunriseCheck = findViewById(R.id.other_Check);
         boolean sunrise = sunriseCheck.isChecked();
 
         CheckBox daytimeCheck = findViewById(R.id.daytime_Check);
