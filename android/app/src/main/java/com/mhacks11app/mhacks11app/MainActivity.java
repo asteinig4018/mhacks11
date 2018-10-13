e4cd03f54f3f8ce466e81abd9e2e9d6f3ce8a58a
asteinig4018 <asteinig18@gmail.com>
2018-10-13 03:13:23 -0400

    maps?maybe

--- android/app/src/main/java/com/mhacks11app/mhacks11app/MainActivity.java
+++ android/app/src/main/java/com/mhacks11app/mhacks11app/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +19,8 @@ public class MainActivity extends AppCompatActivity {
     }
     public void nextActivity(View view){
         Intent intent = new Intent(this,MapsActivity.class);
+        //EditText editText= (EditText) findViewById(R.id.editText);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);}
 }
